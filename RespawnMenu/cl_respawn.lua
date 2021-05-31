ae4cd1a35668f1e47c9fee0a38b787c5d1b6bde3
Jack Allen <73309719+ClonedDev@users.noreply.github.com>
2021-05-31 12:55:58 +0100

    Added: Respawn Menu

---
+++ RespawnMenu/cl_respawn.lua
@@ -0,0 +1,83 @@
+RMenu.Add('RespawnMenu', 'main', RageUI.CreateMenu("Respawn Menu", "~b~Cloned's Respawn Menu", 1350, 50))
+
+RageUI.CreateWhile(1.0, RMenu:Get('RespawnMenu', 'main'), nil, function()
+
+    RageUI.IsVisible(RMenu:Get('RespawnMenu', 'main'), true, false, true, function()
+        if cfg.freeze == true then
+            FreezeEntityPosition(PlayerPedId(-1), true)
+        end 
+
+        for i , p in pairs(cfg.options) do 
+            RageUI.Button(p.name , nil, "", true, function(Hovered, Active, Selected)
+
+                if Selected then
+                    SetEntityCoords(PlayerPedId(), p.location)
+                end
+
+            end)
+        end
+        
+    end, function()
+
+    end)
+end)
+
+isInMenu = false
+currentAmmunition = nil
+Citizen.CreateThread(function() 
+    while true do
+        local v1 = cfg.coords 
+        if isInMenu == false then
+            if isInArea(v1, 1.4) then 
+                currentAmmunition = k
+                RageUI.Visible(RMenu:Get("RespawnMenu", "main"), true)
+                isInMenu = true
+                currentAmmunition = k 
+            end
+        end
+        if cfg.marker == true then
+            if isInArea(v1, 100.0) then 
+                DrawMarker(2, v1+1 - 0.98, 0, 0, 0, 0, 0, 0, 0.8, 0.8, 0.8, 0, 153, 255, 150, 0, 0, 0, 0, 0, 0, 0)
+            end
+        end
+        if isInArea(v1, 1.4) == false and isInMenu and k == currentAmmunition then
+            RageUI.Visible(RMenu:Get("RespawnMenu", "main"), false)
+            isInMenu = false
+            currentAmmunition = nil
+            if cfg.freeze == true then
+                FreezeEntityPosition(PlayerPedId(-1), false)
+            end
+        end
+        Citizen.Wait(0)
+    end
+end)
+
+-- [When Pressing Backspace it doesn't exit]
+if cfg.freeze == false then
+    Citizen.CreateThread(function()
+    	while true do
+    		Citizen.Wait(0)
+    		if isInMenu then
+                RageUI.Visible(RMenu:Get("RespawnMenu", "main"), true)
+    		else
+    			FreezeEntityPosition(PlayerPedId(-1), false)
+    			Citizen.Wait(500)
+    		end
+    	end
+    end)
+end
+
+
+-- [Leave This]
+function isInArea(v, dis) 
+    
+    if #(GetEntityCoords(PlayerPedId(-1)) - v) <= dis then  
+        return true
+    else 
+        return false
+    end
+end
+
+
+
+
---
+++ RespawnMenu/fxmanifest.lua
@@ -0,0 +1,41 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+dependencies {
+  "RageUI",
+}
+
+
+client_scripts {
+    "@RageUI/src/RMenu.lua",
+    "@RageUI/src/menu/RageUI.lua",
+    "@RageUI/src/menu/Menu.lua",
+    "@RageUI/src/menu/MenuController.lua",
+    "@RageUI/src/components/Audio.lua",
+    "@RageUI/src/components/Rectangle.lua",
+    "@RageUI/src/components/Screen.lua",
+    "@RageUI/src/components/Sprite.lua",
+    "@RageUI/src/components/Text.lua",
+    "@RageUI/src/components/Visual.lua",
+    "@RageUI/src/menu/elements/ItemsBadge.lua",
+    "@RageUI/src/menu/elements/ItemsColour.lua",
+    "@RageUI/src/menu/elements/PanelColour.lua",
+    "@RageUI/src/menu/items/UIButton.lua",
+    "@RageUI/src/menu/items/UICheckBox.lua",
+    "@RageUI/src/menu/items/UIList.lua",
+    "@RageUI/src/menu/items/UIProgress.lua",
+    "@RageUI/src/menu/items/UISlider.lua",
+    "@RageUI/src/menu/items/UISliderHeritage.lua",
+    "@RageUI/src/menu/items/UISliderProgress.lua",
+    "@RageUI/src/menu/panels/UIColourPanel.lua",
+    "@RageUI/src/menu/panels/UIGridPanel.lua",
+    "@RageUI/src/menu/panels/UIGridPanelHorizontal.lua",
+    "@RageUI/src/menu/panels/UIPercentagePanel.lua",
+    "@RageUI/src/menu/panels/UIStatisticsPanel.lua",
+    "@RageUI/src/menu/windows/UIHeritage.lua",
+    "cl_respawn.lua",
+}
+
+shared_scripts {
+  "config.lua"
+}
